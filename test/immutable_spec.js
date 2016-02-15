093f28d5e34d5b915c54da497a12991b5f04cb47
kmchen <kuangming_chen@htc.com>
2016-02-15 11:15:20 +0800

    Add immutable tests

---
+++ test/immutable_spec.js
@@ -0,0 +1,43 @@
+import {expect} from 'chai';
+import {Map, List} from 'immutable';
+
+describe('immutability', () => {
+  describe('a number', () => {
+    function increment (currentState) {
+      return currentState+1; 
+    }
+
+    it('is immutable', () => {
+      let state = 42;
+      let nextState = increment(state);
+      expect(nextState).to.equal(43);
+      expect(state).to.equal(42);
+    });
+  })
+
+  describe('a list', () => {
+    function addMovie(currentState, movie){
+      return currentState.push(movie); 
+    } 
+
+    it('is immutable', () => {
+      let state = List.of("movie one", "movie two")
+      let nextState = addMovie(state, 'movie three');
+      expect(nextState).to.equal(List.of('movie one', 'movie two', 'movie three'));
+      expect(state).to.equal(List.of('movie one', 'movie two'));
+    });
+  });
+
+  describe('a map', () => {
+    function addMovieMap(currentState, movie){
+      return currentState.set('movies',currentState.get('movies').push(movie)); 
+    } 
+
+    it('is immutable', () => {
+      let state = Map({'movies': List.of("movie one", "movie two")})
+      let nextState = addMovieMap(state, 'movie three');
+      expect(nextState).to.equal(Map({'movies': List.of('movie one', 'movie two', 'movie three')}));
+      expect(state).to.equal(Map({'movies': List.of('movie one', 'movie two')}));
+    });
+  });
+});
