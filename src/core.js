0afc25f1308dd2ab3d57f53d04fea3affbc69fcc
kmchen <kuangming_chen@htc.com>
2016-03-01 12:54:25 +0800

    Add voting for tally

--- src/core.js
+++ src/core.js
@@ -1,3 +1,17 @@
+import {Map, List}  from 'immutable';
+
 export function setEntries(state, entries){
-  return state.set('entries', entries)
+  return state.set('entries', List(entries))
+}
+
+export function next(state){
+  let entries = state.get('entries')
+  return state.merge({
+    'vote': Map({pair: entries.take(2)}),
+    'entries': entries.skip(2),
+  })
+}
+
+export function tally(state, movie){
+  return state.updateIn(['vote', 'tally', movie], 0, tally => tally + 1)
 }
