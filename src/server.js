2ab22ddda6caa5bd1f4a18b353ee659ba17e6068
kmchen <kmchen@chenkuaingdeair.htctaipei.htc.com.tw>
2016-03-11 12:13:09 +0800

    Receiving remote actions

--- src/server.js
+++ src/server.js
@@ -4,10 +4,14 @@ export function startServer(store) {
   const io = new Server().attach(8090);
 
   // Broadcast JSON-serialzed snapshot of the state to all active
-  // Socket.io connection
+  // Socket.io connections
   store.subscribe(() => io.emit('state', store.getState().toJS()));
 
+  // For every new connection
   io.on('connection', (socket) => {
+    // Send current state
     socket.emit('state', store.getState().toJS());
+    // Allowing clients to feed actions
+    socket.on('action', store.dispatch.bind(store));
   })
 }
